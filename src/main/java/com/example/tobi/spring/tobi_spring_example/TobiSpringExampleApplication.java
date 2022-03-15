7c2047fc4324d3f67aeec746aab96727d6edb114
Lstamp <lozenkrantz@gmail.com>
2022-03-16 02:13:23 +0900

    first commit

---
+++ src/main/java/com/example/tobi/spring/tobi_spring_example/TobiSpringExampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.tobi.spring.tobi_spring_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobiSpringExampleApplication.class, args);
+		System.out.println("Hello World!!!!!");
+	}
+
+}
---
+++ src/test/java/com/example/tobi/spring/tobi_spring_example/TobiSpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tobi.spring.tobi_spring_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobiSpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
